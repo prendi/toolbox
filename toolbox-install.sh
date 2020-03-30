7d54d5bbbc876a053ab25f009f458bc25ac8049d
Gonçalo Prendi <goncalo.prendi25@gmail.com>
2020-03-30 12:33:41 +0100

    Add sdkman, autojump, scala, jq, shutter and npm

--- toolbox-install.sh
+++ toolbox-install.sh
@@ -10,19 +10,24 @@ sudo apt-get update
 # Install with APT
 apt-get install python-pip htop vim lnav zsh git \
   apt-transport-https ca-certificates curl gnupg-agent software-properties-common \
-  terminator docker-ce docker-ce-cli containerd.io awscli autojump
+  terminator docker-ce docker-ce-cli containerd.io awscli autojump scala jq shutter \
+  npm
 
 # Install with snap
 snap install slack --classic
 
 # Other installs
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+curl -s "https://get.sdkman.io" | bash
 
 CURRENT_VERSION=2.25.0
 wget https://github.com/Versent/saml2aws/releases/download/v${CURRENT_VERSION}/saml2aws_${CURRENT_VERSION}_linux_amd64.tar.gz
 tar -xzvf saml2aws_${CURRENT_VERSION}_linux_amd64.tar.gz -C ~/.local/bin
 chmod u+x ~/.local/bin/saml2aws
 
+sdk install sbt
+sdk install java 8.0.242.j9-adpt
+
 # Setup
-export PATH=$HOME/.local/bin:$PATH
-source /usr/share/autojump/autojump.sh
+echo "export PATH=$HOME/.local/bin:$PATH" >> ~/.zshrc
+echo "source /usr/share/autojump/autojump.sh" >> ~/.zshrc
