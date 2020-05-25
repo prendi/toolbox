cd961840659308e5b420c7c443459390aa95a48c
Gonçalo Prendi <goncalo.prendi25@gmail.com>
2020-05-25 16:59:10 +0100

    Add missing alias for git add

--- git-config.sh
+++ git-config.sh
@@ -1,7 +1,11 @@
-git config --global alias.co checkout                                                                                                                                                                                                                git:(develop)
+#!/bin/bash
+
+git config --global alias.co checkout
 git config --global alias.b branch
 git config --global alias.c commit
 git config --global alias.s status
-git config --global alias.st stash                                                                                                                                                                                                                   git:(develop)
-git config --global alias.d diff                                                                                                                                                                                                                     git:(develop)
+git config --global alias.st stash
+git config --global alias.d diff
 git config --global alias.r reset
+git config --global alias.a add
+
